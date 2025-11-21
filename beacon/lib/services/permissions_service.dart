373fcd805150f9e50492c8fdeff2c3a729d737cf
AhmedFaek <ahmed22012501@gmail.com>
2025-11-21 21:36:54 +0200

    new permission added

--- beacon/lib/services/permissions_service.dart
+++ beacon/lib/services/permissions_service.dart
@@ -2,9 +2,17 @@ import 'package:permission_handler/permission_handler.dart';
 
 class PermissionService {
   static Future<bool> requestPermissions() async {
+    print('[PermissionService] Requesting permissions...');
+    
     var location = await Permission.location.request();
+    print('[PermissionService] Location permission: ${location.toString()}');
+    
     var nearby = await Permission.nearbyWifiDevices.request();
+    print('[PermissionService] Nearby WiFi devices permission: ${nearby.toString()}');
 
-    return location.isGranted && nearby.isGranted;
+    bool granted = location.isGranted && nearby.isGranted;
+    print('[PermissionService] All permissions granted: $granted');
+    
+    return granted;
   }
 }
